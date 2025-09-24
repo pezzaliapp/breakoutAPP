e559ccb057a7bcdb4a4a9fe61ac4b3ba36fc326c
pezzaliapp <pezzalialessandro@gmail.com>
2025-09-24 21:53:02 +0200

    Add files via upload

---
+++ sw-v5.js
@@ -0,0 +1,31 @@
+const CACHE = 'breakout-v5';
+const ASSETS = [
+  './',
+  './index.html?v=5',
+  './style.v5.css',
+  './app.v5.js',
+  './manifest.webmanifest?v=5',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e=>{
+  const url = new URL(e.request.url);
+  if(url.pathname.endsWith('/index.html')) {
+    // normalize to versioned page
+    return e.respondWith(caches.match('./index.html?v=5').then(r=>r||fetch(e.request)));
+  }
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request)));
+});
